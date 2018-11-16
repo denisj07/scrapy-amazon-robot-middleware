e76ab6eff7d3ae9366f6ea68b97c33c791086ee3
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-11-16 03:29:38 -0800

    Fixed setup.py egg data error

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ setup(
     name='scrapy-amazon-robot-middleware',
     version=version,
     packages=find_packages(),
-    url='https://github.com/ziplokk1/scrapy-amazon-robot-middleware',
+    url='https://github.com/AbdulYahya/scrapy-amazon-robot-middleware',
     license='LICENSE.txt',
     author='Mark Sanders',
     author_email='sdscdeveloper@gmail.com',
     install_requires=REQUIREMENTS,
     description='Scrapy middleware module which uses image parsing to submit a captcha response to amazon.',
-    include_package_data=True
+    include_package_data=false
 )
