3382a089c2404d850421ab7b053ef916ce6ab764
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-11-16 03:37:54 -0800

    Fixed typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 version = '0.1.0'
 
 REQUIREMENTS = [
-    'beautifulsoup',
+    'beautifulsoup4',
     'scrapy',
     'pillow',
     'requests'
@@ -12,7 +12,7 @@ REQUIREMENTS = [
 setup(
     name='scrapy-amazon-robot-middleware',
     version=version,
-    # packages=find_packages(),
+    packages=find_packages(),
     url='https://github.com/AbdulYahya/scrapy-amazon-robot-middleware',
     license='LICENSE.txt',
     author='Mark Sanders',
